c12f78378f22fdb37a657b8f77bb48d0672333c7
ShiroKatake <tgkhang262@gmail.com>
2021-11-23 00:51:40 +1100

    UPDATE: Implemented progress bar

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,11 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import "./App.css";
 import Button from "./components/Button";
 import Card from "./components/Card";
-import { getInfos, getSuggestions, downloadFileFromUrl } from "./utils/API";
-import { getDownloadUrl, isYtUrl } from "./utils/helpers";
-// import { ProgressBar } from "react-bootstrap";
+import { getInfos, getSuggestions } from "./utils/API";
+import { host, isYtUrl, getDownloadUrl, extractFileName, isJson, isUid, waitForOpenConnection, toMB } from "./utils/helpers";
+import { ProgressBar } from "react-bootstrap";
+import axios from "axios";
 
 const formats = [
   {
@@ -32,6 +33,66 @@ const App = () => {
   const [suggestions, setSuggestions] = useState<any>([]);
   const [currentVideoInfo, setCurrentVideoInfo] = useState<any>(null);
   const [focus, setFocus] = useState(false);
+
+  const [downloadProgress, setDownloadProgress] = useState(0);
+  const [hidden, setHidden] = useState(true);
+
+  const [downloaded, setDownloaded] = useState(0);
+  const [totalSize, setTotalSize] = useState(1);
+
+  const downloadFileFromUrl = async (videoDownloadUrl: string) => {
+    // Create WebSocket connection.
+    const socket = new WebSocket(`ws://${host.replace(/^https?:\/\//i, "")}`);
+
+    let uid = "";
+
+    // Listen for messages
+    socket.addEventListener("message", event => {
+      if (isUid(event.data)) {
+        uid = isUid(event.data);
+      }
+      if (isJson(event.data)) {
+        const downloadProgress = JSON.parse(event.data);
+        setDownloadProgress((downloadProgress.downloaded / downloadProgress.total) * 100);
+        setDownloaded(downloadProgress.downloaded);
+        setTotalSize(downloadProgress.total);
+        setHidden(false);
+      }
+    });
+
+    if (socket.readyState !== socket.OPEN) {
+      try {
+        await waitForOpenConnection(socket);
+        socket.send(uid);
+      } catch (err) {
+        console.error(err);
+      }
+    } else {
+      socket.send(uid);
+    }
+
+    try {
+      axios({
+        url: videoDownloadUrl,
+        method: "POST",
+        responseType: "blob",
+        data: { uid: uid },
+      }).then(response => {
+        const fileName = extractFileName(response.headers["content-disposition"]);
+        const url = window.URL.createObjectURL(new Blob([response.data]));
+
+        const link = document.createElement("a");
+        link.href = url;
+        link.setAttribute("download", fileName);
+        document.body.appendChild(link);
+
+        link.click();
+        link.remove();
+      });
+    } catch (error) {
+      console.log(error);
+    }
+  };
 
   const checkInput = () => {
     if (isYtUrl(inputText)) {
@@ -64,6 +125,15 @@ const App = () => {
     }
   };
 
+  useEffect(() => {
+    if (downloadProgress === 100) {
+      setTimeout(() => {
+        setHidden(true);
+        setDownloadProgress(0);
+      }, 5000);
+    }
+  }, [downloadProgress]);
+
   return (
     <>
       <main className="container">
@@ -84,7 +154,14 @@ const App = () => {
             />
           </div>
           <br />
-          {/* <ProgressBar striped variant="success" now={40} label="Initializing . . ." style={{ width: "85%", height: "20px" }} /> */}
+          <ProgressBar
+            hidden={hidden}
+            striped
+            variant="success"
+            now={downloadProgress}
+            label={`${downloadProgress !== 100 ? "Fetching . . ." : "Ready! "} ${toMB(downloaded)}MB /${toMB(totalSize)}MB`}
+            style={{ width: "85%", height: "30px", lineHeight: "30px" }}
+          />
           <ul className="format-list">
             {formats.map(format => {
               return (
--- src/utils/helpers.js
+++ src/utils/helpers.js
@@ -1,6 +1,6 @@
 // Requires to have the local backend if you wanna run backend
-// export const host = "http://localhost:4000";
-export const host = "https://shirokatake-ytdl-backend.herokuapp.com";
+export const host = "http://localhost:4000";
+// export const host = "https://shirokatake-ytdl-backend.herokuapp.com";
 
 export const getDownloadUrl = (videoId, format = "mp4") => `${host}/download?v=${videoId}&format=${format}`;
 
@@ -18,3 +18,44 @@ export const isYtUrl = url => {
 export const changeFormatStorage = format => {
   localStorage.setItem("format", format);
 };
+
+export const extractFileName = str => {
+  const regex = /.*filename=['"]?([^"]+)/g;
+  return regex.exec(str)[1];
+};
+
+export const isJson = str => {
+  try {
+    JSON.parse(str);
+  } catch (e) {
+    return false;
+  }
+  return true;
+};
+
+export const waitForOpenConnection = socket => {
+  return new Promise((resolve, reject) => {
+    const maxNumberOfAttempts = 10;
+    const intervalTime = 200; //ms
+
+    let currentAttempt = 0;
+    const interval = setInterval(() => {
+      if (currentAttempt > maxNumberOfAttempts - 1) {
+        clearInterval(interval);
+        reject(new Error("Maximum number of attempts exceeded"));
+      } else if (socket.readyState === socket.OPEN) {
+        clearInterval(interval);
+        resolve();
+      }
+      currentAttempt++;
+    }, intervalTime);
+  });
+};
+
+export const isUid = str => {
+  const regex = /[a-zA-Z0-9]{8}-[a-zA-Z0-9]{4}/g;
+  const res = str.match(regex);
+  return res ? res[0] : null;
+};
+
+export const toMB = i => (i / 1024 / 1024).toFixed(2);
