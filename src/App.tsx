ac0e43c3f11d530679b9b5b2cf9426f586ee3ef1
ShiroKatake <tgkhang262@gmail.com>
2021-12-02 17:57:58 +1100

    UPDATE: Separated button and styles into its own component

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import { useState } from "react";
 import "./App.css";
-import Button from "./components/Button";
+import Button from "./components/Button/Button";
 import Card from "./components/Card";
 import { getInfos, getSuggestions, downloadFileFromUrl } from "./utils/API";
 import { host, isYtUrl, getDownloadUrl, isJson, isUid, waitForOpenConnection, toMB } from "./utils/helpers";
@@ -106,80 +106,78 @@ const App = () => {
 
   return (
     <>
-      <main className="container">
-        <section className="search-section">
-          <div className={`input-container ${focus ? "animate" : ""}`}>
-            <input
-              type="text"
-              name="input"
-              id="text"
-              placeholder="Search or paste url"
-              spellCheck={false}
-              autoComplete="off"
-              onBlur={() => setFocus(false)}
-              onFocus={() => setFocus(true)}
-              value={inputText}
-              onChange={e => setInputText(e.target.value)}
-              autoFocus
-            />
-          </div>
-          <br />
-          <ProgressBar
-            hidden={hidden}
-            striped
-            variant="success"
-            now={downloadProgress}
-            label={`${downloadProgress !== 100 ? "Fetching . . ." : "Ready! "} ${toMB(downloaded)}MB /${toMB(totalSize)}MB`}
-            style={{ width: "85%", height: "30px", lineHeight: "30px" }}
+      <section className="search-section">
+        <div className={`input-container ${focus ? "animate" : ""}`}>
+          <input
+            type="text"
+            name="input"
+            id="text"
+            placeholder="Search or paste url"
+            spellCheck={false}
+            autoComplete="off"
+            onBlur={() => setFocus(false)}
+            onFocus={() => setFocus(true)}
+            value={inputText}
+            onChange={e => setInputText(e.target.value)}
+            autoFocus
           />
-          <ul className="format-list">
-            {formats.map(format => {
-              return (
-                <li key={format.id}>
-                  <input
-                    type="radio"
-                    id={format.id}
-                    name="format"
-                    checked={downloadFormat === format.id}
-                    value={format.name}
-                    onChange={e => setDownloadFormat(e.target.value)}
-                  />
-                  <label htmlFor={format.id} className="radio-label">
-                    {format.name}
-                  </label>
-                </li>
-              );
-            })}
-          </ul>
-          <Button disabled={!hidden} onClick={checkInput} />
-        </section>
-        {currentVideoInfo && (
-          <section className="downloading-section">
-            <div>
-              <h2>{currentVideoInfo.title}</h2>
-              <br />
-              <img src={`https://i.ytimg.com/vi/${currentVideoInfo.videoId}/hqdefault.jpg`} alt={currentVideoInfo.title} />
-            </div>
-          </section>
-        )}
-        <section className="suggestions-section">
-          {!!suggestions.length && <h1>Suggestions</h1>}
-          <div className="grid">
-            {suggestions.map((video: any) => {
-              return (
-                <Card
-                  key={video.id}
-                  author={video.author.name}
-                  title={video.title}
-                  videoId={video.id}
-                  thumbnailUrl={video.bestThumbnail.url}
-                  handleDownload={() => download(video.id)}
+        </div>
+        <br hidden={hidden} />
+        <ProgressBar
+          hidden={hidden}
+          striped
+          variant="success"
+          now={downloadProgress}
+          label={`${downloadProgress !== 100 ? "Fetching . . ." : "Ready! "} ${toMB(downloaded)}MB /${toMB(totalSize)}MB`}
+          style={{ width: "85%", height: "30px", lineHeight: "30px" }}
+        />
+        <ul className="format-list">
+          {formats.map(format => {
+            return (
+              <li key={format.id}>
+                <input
+                  type="radio"
+                  id={format.id}
+                  name="format"
+                  checked={downloadFormat === format.id}
+                  value={format.name}
+                  onChange={e => setDownloadFormat(e.target.value)}
                 />
-              );
-            })}
+                <label htmlFor={format.id} className="radio-label">
+                  {format.name}
+                </label>
+              </li>
+            );
+          })}
+        </ul>
+        <Button disabled={!hidden} onClick={checkInput} />
+      </section>
+      {currentVideoInfo && (
+        <section className="downloading-section">
+          <div>
+            <h2>{currentVideoInfo.title}</h2>
+            <br />
+            <img src={`https://i.ytimg.com/vi/${currentVideoInfo.videoId}/hqdefault.jpg`} alt={currentVideoInfo.title} />
           </div>
         </section>
-      </main>
+      )}
+      <section className="suggestions-section">
+        {!!suggestions.length && <h1>Suggestions</h1>}
+        <div className="grid">
+          {suggestions.map((video: any) => {
+            return (
+              <Card
+                key={video.id}
+                author={video.author.name}
+                title={video.title}
+                videoId={video.id}
+                thumbnailUrl={video.bestThumbnail.url}
+                handleDownload={() => download(video.id)}
+              />
+            );
+          })}
+        </div>
+      </section>
       <footer className="footer"></footer>
     </>
   );
--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -1,3 +1,5 @@
+import "./Button.css";
+
 interface IButtonProps {
   onClick: () => void;
   disabled: boolean;
