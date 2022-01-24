44118b1569b44b3d46e5fccbb079558c6b922661
ShiroKatake <tgkhang262@gmail.com>
2022-01-24 19:57:05 +1100

    ADD: Unit test for generateDownloadUrl

---
+++ src/utils/helper/generateDownloadUrl.test.ts
@@ -0,0 +1,14 @@
+import { generateDownloadUrl } from "./generateDownloadUrl";
+
+describe("Generate Download URL", () => {
+  it("should generate urls for mp3 and mp4", () => {
+    expect(generateDownloadUrl("videoId", "mp3")).toBe("http://localhost:4000/download?v=videoId&format=mp3");
+    expect(generateDownloadUrl("differentId", "mp4")).toBe("http://localhost:4000/download?v=differentId&format=mp4");
+  });
+
+  it("should not generate urls for any other input", () => {
+    expect(() => {
+      generateDownloadUrl("differentId", "ma7dfe");
+    }).toThrow("Invalid url.");
+  });
+});
