9c9901d604c9e6c7e8a58aa5d45525682be04b06
ShiroKatake <tgkhang262@gmail.com>
2022-05-15 18:36:59 +1000

    fix(progress text): now only has 2 types of messages

--- src/utils/helper/generateProgressText.test.ts
+++ src/utils/helper/generateProgressText.test.ts
@@ -3,7 +3,6 @@ import { generateProgressText } from "./generateProgressText";
 describe("Generate Progress Text", () => {
   it("should generate correct text", () => {
     expect(generateProgressText(30)).toBe("Fetching . . . 30%");
-    expect(generateProgressText(80)).toBe("Preparing download . . . 80%");
-    expect(generateProgressText(100)).toBe("Ready! 100%");
+    expect(generateProgressText(100)).toBe("Done! Preparing download . . .");
   });
 });
